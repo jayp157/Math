434a8b49a4b86eba851f18bca4610c500b925aa1
jayp157 <jayp157@usf.edu>
2024-09-27 15:16:51 -0400

    Add files via upload

---
+++ Math.R
@@ -0,0 +1,17 @@
+A <- matrix(1:100, nrow=10)
+B <- matrix(1:1000, nrow=10)
+
+determinant_A <- det(A)
+determinant_A
+
+
+inverse_A <- solve(A)
+inverse_A
+
+determinant_B <- det(B)
+determinant_B
+
+inverse_B <- solve(B)
+inverse_B
+
+
